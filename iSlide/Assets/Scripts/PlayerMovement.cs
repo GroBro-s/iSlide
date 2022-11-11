ba1d1df15e69b7fd1a30907feb6b9478ebe7c8d6
ShowLagg <ShowLaggGitHub@gmail.com>
2022-11-11 16:52:35 +0100

    jump mechanisme + achtergrond toegevoegd
    De achtergrond beweegt nu achter de player langs, en de player kan springen.

--- iSlide/Assets/Scripts/PlayerMovement.cs
+++ iSlide/Assets/Scripts/PlayerMovement.cs
@@ -4,27 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Joystick joystick;
-    private float horizontalMove, verticalMove;
+    bool isGrounded = false;
+    private Rigidbody2D rb;
+    public float jumpHeight;
+    //private float verticalMoveInput;
+
+    //public Joystick joystick;
+    //private float horizontalMove, verticalMove;
     [SerializeField]
-    private float runSpeed;
+    //private float runSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //input player
-        horizontalMove = joystick.Horizontal;
-        verticalMove = joystick.Vertical;
+        //horizontalMove = joystick.Horizontal;
+        //verticalMove = joystick.Vertical;
     }
 
     void FixedUpdate()
     {
         //output player
-        transform.Translate(horizontalMove * runSpeed * Time.deltaTime, verticalMove * runSpeed * Time.deltaTime, 0);
+        //transform.Translate(horizontalMove * runSpeed * Time.deltaTime, verticalMove * runSpeed * Time.deltaTime, 0);
+    }
+
+    /*void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Vertical");
+        rb.Velocity = new Vector2(VerticalMoveInput)
+    }*/
+    public void Jump()
+    {
+        if(isGrounded)
+        {
+            isGrounded = false;
+            rb.velocity = Vector2.up * jumpHeight;
+            print("Jump");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Floor")
+        {
+            print("Crashed");
+            isGrounded = true; 
+        }
+
     }
 }
+        /*Vector2 move = new Vector2(0, 3);
+        transform.Translate(move);*/
