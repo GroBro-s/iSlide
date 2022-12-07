103ea8f14c08bdf53782750d7664c5a0b7c6d6de
ShowLagg <ShowLaggGitHub@gmail.com>
2022-12-07 15:34:32 +0100

    bijna compleet switch

--- iSlide/Assets/Scripts/PlayerMovement.cs
+++ iSlide/Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,24 @@ public class PlayerMovement : MonoBehaviour
     bool isGrounded = false;
     private Rigidbody2D rb;
     public float jumpHeight;
+    public GameObject gameManager;
+    private SceneSwitcher sceneSwitcher;
+    private bool tableScene;
 
-
-    void Start()
+    void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();  
+        sceneSwitcher = gameManager.GetComponent<SceneSwitcher>();     
+    }
+
+    void Update()
+    {
+        tableScene = sceneSwitcher.tableScene;
     }
 
     public void Jump()
     {
-        if(isGrounded)
+        if(isGrounded && tableScene)
         {
             isGrounded = false;
             rb.velocity = Vector2.up * jumpHeight;
--- iSlide/Assets/Scripts/SceneSwitcher.cs
+++ iSlide/Assets/Scripts/SceneSwitcher.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameObject gameManager;
+    private ObstacleSpawner obstacleSpawner;
+    private UpgradeSpawner upgradeSpawner;
+    [SerializeField] private GameObject backGround;
+    private BackGroundScroll backGroundScroll;
+    [SerializeField] private GameObject floor;
+    private BackGroundScroll floorScroll;  
+    public GameObject player;
+    private PlayerMovement playerMovement;
+    public bool tableScene;
+    private int tableTime;
+    private int moveSpeed;
+    private bool floorSpawned;
+
+
+    void Awake()
+    {
+        floorSpawned = true;
+        gameManager = this.gameObject;
+        obstacleSpawner = gameManager.GetComponent<ObstacleSpawner>();
+        upgradeSpawner = gameManager.GetComponent<UpgradeSpawner>();
+        playerMovement = player.GetComponent<PlayerMovement>();
+        backGroundScroll = backGround.GetComponent<BackGroundScroll>();
+        floorScroll = floor.GetComponent<BackGroundScroll>();
+    }
     void Start()
     {
-        
+        StartCoroutine("SwitchToAir");
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        floor.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        if(floorSpawned && floor.transform.position.x < -30)
+        {
+            Destroy(floor);
+            floorSpawned = false;
+        }
+    }
+
+    IEnumerator SwitchToAir()
+    {
+        tableTime = 2;
+        yield return new WaitForSeconds(tableTime);
+        tableScene = false;
+        obstacleSpawner.StopCoroutine("SpawnObstacle");
+        upgradeSpawner.StopCoroutine("SpawnUpgrade");
+
+        yield return new WaitForSeconds(1);
+        moveSpeed = 11;
+        floorScroll.xVelocity = 0;
+        player.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        yield return new WaitForSeconds(2);
+        backGroundScroll.xVelocity = 0;
+        backGroundScroll.yVelocity = -4;
     }
 }
