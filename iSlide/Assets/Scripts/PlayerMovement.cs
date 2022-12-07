b96b0fa54bf514f492a82cc54018a088ef5ddf3b
Sp1derPr1me <luckygameshf@gmail.com>
2022-12-07 20:09:55 +0100

    animationprogress

--- iSlide/Assets/Scripts/PlayerMovement.cs
+++ iSlide/Assets/Scripts/PlayerMovement.cs
@@ -7,31 +7,41 @@ public class PlayerMovement : MonoBehaviour
     bool isGrounded = false;
     private Rigidbody2D rb;
     public float jumpHeight;
-
+    public Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+
     public void Jump()
     {
         if(isGrounded)
         {
             isGrounded = false;
+            animator.SetBool("IsJumping", false);
             rb.velocity = Vector2.up * jumpHeight;
+            
         }
+    }
+
+    public void OnLanding ()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.tag == "Floor")
         {
-            isGrounded = true; 
+            isGrounded = true;
+            animator.SetBool("IsJumping", true);
         }
         if(other.gameObject.tag == "Obstacle")
         {
             isGrounded = true;
+            animator.SetBool("IsJumping", true);
         }
     }
 }
